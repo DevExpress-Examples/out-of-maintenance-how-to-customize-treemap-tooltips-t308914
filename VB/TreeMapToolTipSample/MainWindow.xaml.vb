51bb173a18958db7345b545e1fd7f61868a1a8c5
DevExpressExampleBot <fakeEmail>
2022-12-18 18:11:46 +0400

    Source auto update [skip ci]

--- VB/TreeMapToolTipSample/MainWindow.xaml.vb
+++ VB/TreeMapToolTipSample/MainWindow.xaml.vb
@@ -1,40 +1,33 @@
-﻿Imports DevExpress.Xpf.TreeMap
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
+Imports DevExpress.Xpf.TreeMap
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
 Imports System.Windows.Markup
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace TreeMapToolTipSample
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-	<ContentProperty("Position")>
-	Public Class PositionWrapper
-		Public Property PositionName() As String
-		Public Property Position() As ToolTipPosition
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 
-	Public Class OpenModeWrapper
-		Public Property OpenModeName() As String
-		Public Property OpenMode() As ToolTipOpenMode
-	End Class
+    <ContentProperty("Position")>
+    Public Class PositionWrapper
+
+        Public Property PositionName As String
+
+        Public Property Position As ToolTipPosition
+    End Class
+
+    Public Class OpenModeWrapper
+
+        Public Property OpenModeName As String
+
+        Public Property OpenMode As ToolTipOpenMode
+    End Class
 End Namespace
